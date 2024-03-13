fcdc743faf632d3f437e15c07a1fb1e06bcb523c
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-03-14 00:14:29 +0530

    fix typo

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -5,7 +5,7 @@ $dbusername = "root";
 $dbpassword = "";
 
 try{
-    $pdo = new PDO($dns, $dbusername, $dbpassword);
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $error){
     echo "Connection Failed: ". $error->getMessage();
