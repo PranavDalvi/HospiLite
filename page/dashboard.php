d1d3577eda21b8dcab3b9377a7a3bb59d0d7dd92
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-03-15 09:16:50 +0530

    add logout button on dashboard.php

---
+++ page/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="../includes/logout.inc.php" method="post">
+        <button>Logout</button>
+    </form>
+
+</body>
+
+</html>
